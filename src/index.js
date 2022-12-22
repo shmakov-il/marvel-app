227d4a6415ff5f006e9f2c486a3a839a9a67edad
Ilya Shmakov <shmakov.il.e@gmail.com>
2022-12-22 14:10:08 +0300

    create change randomChar in time load page

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
 import './style/style.scss';
-import MarvelService from "./services/MarvelService";
-
-// const marvelServices = new MarvelService()
-// marvelServices.getCharactersAll().then(res => res.data.results.forEach(item => console.log(item.name)))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -9,12 +9,24 @@ class MarvelService {
         return await res.json();
     }
 
-    getCharactersAll = () => {
-        return this.getResource(`${this._apiBase}/characters?limit=9&offset=200&${this._apiKey}`);
+    getCharactersAll = async () => {
+        const res = await this.getResource(`${this._apiBase}/characters?limit=9&offset=200&${this._apiKey}`);
+        return res.data.results.map(this._transformChar)
     }
 
-    getCharacter = (id) => {
-        return this.getResource(`${this._apiBase}/characters/${id}?${this._apiKey}`);
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}/characters/${id}?${this._apiKey}`);
+        return this._transformChar(res.data.results[0]);
+    }
+
+    _transformChar = (char) => {
+        return {
+            name: char.name,
+            description: char.description ? `${char.description.slice(0, 170)}...` : 'There is no description for this character',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
     }
 }
 
